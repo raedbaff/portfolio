f0b6b3a83a9264a31d7e07b453defe6a9182786d
raed <raed.baffoun@polytechnicien.tn>
2024-05-10 14:00:59 +0100

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     basePath:"/portfolio",
     output:"export",
-    reactStrictMode:true
+    reactStrictMode:true,
+    images: { unoptimized: true }
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hello world</h1>
-  );
+    <>
+      <h1>hello world</h1>
+      <Image
+      src="/portfolio/1.jpg" // Path to your image in the `public` directory
+      alt="Image description"
+      width={500} // Optional: Width of the image in pixels
+      height={300} // Optional: Height of the image in pixels
+    />
+     <Image
+      src="/portfolio/amedBg.png" // Path to your image in the `public` directory
+      alt="Image description"
+      width={500} // Optional: Width of the image in pixels
+      height={300} // Optional: Height of the image in pixels
+    />
+    
+    </>
+  )
 }
